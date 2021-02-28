2e50eea915d4334e8f9186f1ffa6d381faac12c2
Julian Hüppauff <julian@hueppauff.com>
2021-02-28 14:06:46 +0100

    fixed string

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,6 +4,6 @@ module.exports = {
 
     execute(client, message) {
         const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply("Pong! This message had a latency of ${timeTaken}ms.");
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
     }
 }
