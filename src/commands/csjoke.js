32a696f1ba5d835a8f33c48aedaf38d8f57362f5
Julian Hüppauff <julian@hueppauff.com>
2021-03-02 22:13:47 +0100

    fixed skill

--- src/commands/csjoke.js
+++ src/commands/csjoke.js
@@ -4,145 +4,133 @@ const { promises } = require('dns');
 const configuration = require('../config.json');
 
 function synthesizeSpeech(text, output, language, voiceName) {
-  let speechConfig;
-  if (
-    configuration.COGNITIVE_SERVICES_APIKEY &&
-    configuration.COGNITIVE_SERVICES_APIKEY != '' &&
-    configuration.COGNITIVE_SERVICES_APIKEY != 'token'
-  ) {
-    speechConfig = sdk.SpeechConfig.fromSubscription(
-      configuration.COGNITIVE_SERVICES_APIKEY,
-      configuration.COGNITIVE_SERVICES_REGION,
+    let speechConfig;
+    if (
+        configuration.COGNITIVE_SERVICES_APIKEY &&
+        configuration.COGNITIVE_SERVICES_APIKEY != '' &&
+        configuration.COGNITIVE_SERVICES_APIKEY != 'token'
+    ) {
+        speechConfig = sdk.SpeechConfig.fromSubscription(
+            configuration.COGNITIVE_SERVICES_APIKEY,
+            configuration.COGNITIVE_SERVICES_REGION,
+        );
+    } else if (
+        process.env.COGNITIVE_SERVICES_APIKEY &&
+        process.env.COGNITIVE_SERVICES_APIKEY != ''
+    ) {
+        speechConfig = sdk.SpeechConfig.fromSubscription(
+            process.env.COGNITIVE_SERVICES_APIKEY,
+            process.env.COGNITIVE_SERVICES_REGION,
+        );
+    } else {
+        console.log('No authentication token present');
+        return;
+    }
+
+    speechConfig.speechSynthesisLanguage = language;
+    speechConfig.speechSynthesisVoiceName = voiceName;
+    speechConfig.speechSynthesisOutputFormat =
+        sdk.SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3;
+
+    const audioConfig = sdk.AudioConfig.fromAudioFileOutput(
+        path.join(__dirname, `./temp/${output}`),
     );
-  } else if (
-    process.env.COGNITIVE_SERVICES_APIKEY &&
-    process.env.COGNITIVE_SERVICES_APIKEY != ''
-  ) {
-    speechConfig = sdk.SpeechConfig.fromSubscription(
-      process.env.COGNITIVE_SERVICES_APIKEY,
-      process.env.COGNITIVE_SERVICES_REGION,
+
+    let synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+    synthesizer.speakTextAsync(
+        text,
+        (result) =>
+        new promises((resolve, reject) => {
+            if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+                console.log('synthesis finished.');
+                resolve();
+            } else {
+                console.error(
+                    `Speech synthesis canceled, ${result.errorDetails}\nDid you update the subscription info?`,
+                );
+
+                reject();
+            }
+            synthesizer.close();
+            synthesizer = undefined;
+        }),
+        (error) => {
+            console.trace(`err - ${err}`);
+            synthesizer.close();
+            synthesizer = undefined;
+        },
     );
-  } else {
-    console.log('No authentication token present');
-    return;
-  }
 
-  speechConfig.speechSynthesisLanguage = language;
-  speechConfig.speechSynthesisVoiceName = voiceName;
-  speechConfig.speechSynthesisOutputFormat =
-    sdk.SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3;
-
-  const audioConfig = sdk.AudioConfig.fromAudioFileOutput(
-    path.join(__dirname, `./temp/${output}`),
-  );
-
-  let synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
-
-  synthesizer.speakTextAsync(
-    text,
-    (result) =>
-      new promises((resolve, reject) => {
-        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-          console.log('synthesis finished.');
-          resolve();
-        } else {
-          console.error(
-            `Speech synthesis canceled, ${result.errorDetails}\nDid you update the subscription info?`,
-          );
-
-          reject();
-        }
-        synthesizer.close();
-        synthesizer = undefined;
-      }),
-    (error) => {
-      console.trace(`err - ${err}`);
-      synthesizer.close();
-      synthesizer = undefined;
-    },
-  );
-
-  console.log('Finished Synthesis');
+    console.log('Finished Synthesis');
 }
 
 module.exports = {
-  name: 'csjoke',
+    name: 'csjoke',
 
-  async execute(client, message) {
-    const line1 =
-      'Dear son; Your mother and I love you very much, and we miss you dearly ever since you went to prison. I especially miss you now that spring is here, and it is time to plow the fields. The ground is hard, and my back is old. I am afraid I will never be able to plant the crops in time. Your loving father';
-    const line2 =
-      'Dear Dad; Do not dig in the field. That is where I hide that thing. You know I can not say what it is because they read our mail. Just do not dig out there. Your son';
-    const line3 =
-      'Dear son; The cops came out and dug up my fields. They said they were looking for something. Thanks, son. It looks like I will get the crops planted. Your loving and grateful father';
+    async execute(client, message) {
+        const line1 =
+            'Dear son; Your mother and I love you very much, and we miss you dearly ever since you went to prison. I especially miss you now that spring is here, and it is time to plow the fields. The ground is hard, and my back is old. I am afraid I will never be able to plant the crops in time. Your loving father';
+        const line2 =
+            'Dear Dad; Do not dig in the field. That is where I hide that thing. You know I can not say what it is because they read our mail. Just do not dig out there. Your son';
+        const line3 =
+            'Dear son; The cops came out and dug up my fields. They said they were looking for something. Thanks, son. It looks like I will get the crops planted. Your loving and grateful father';
 
-    try {
-      require.resolve('@discordjs/opus');
-    } catch (err) {
-      console.error('> "discordjs/opus" is not installed.');
-      try {
-        require.resolve('opusscript');
-      } catch (err2) {
-        return console.error('> "opusscript" is not installed.');
-      }
-      return null;
-    }
+        try {
+            require.resolve('@discordjs/opus');
+        } catch (err) {
+            console.error('> "discordjs/opus" is not installed.');
+            try {
+                require.resolve('opusscript');
+            } catch (err2) {
+                return console.error('> "opusscript" is not installed.');
+            }
+            return null;
+        }
 
-    await synthesizeSpeech(line1, 'output1.wav', 'en-US', 'en-US-GuyNeural');
-    await synthesizeSpeech(line2, 'output2.wav', 'en-GB', 'en-GB-RyanNeural');
-    await synthesizeSpeech(line3, 'output3.wav', 'en-US', 'en-US-GuyNeural');
+        await synthesizeSpeech(line1, 'output1.wav', 'en-US', 'en-US-GuyNeural');
+        await synthesizeSpeech(line2, 'output2.wav', 'en-GB', 'en-GB-RyanNeural');
+        await synthesizeSpeech(line3, 'output3.wav', 'en-US', 'en-US-GuyNeural');
 
-    // Command start
-    // Get members current active voice channel.
-    const voiceChannel = message.member.voice.channel;
+        // Command start
+        // Get members current active voice channel.
+        const voiceChannel = message.member.voice.channel;
 
-    // Make sure the voice channel is actually defined (aka, they're inside a voice channel).
-    if (!voiceChannel)
-      return message.reply('You must be in a voice channel first.');
+        // Make sure the voice channel is actually defined (aka, they're inside a voice channel).
+        if (!voiceChannel)
+            return message.reply('You must be in a voice channel first.');
 
-    // We now know the voice channel is defiend, so let's join them.
-    // We also store the connection returned from joining the voice channel.
-    const connection = await voiceChannel.join();
+        // We now know the voice channel is defiend, so let's join them.
+        // We also store the connection returned from joining the voice channel.
+        const connection = await voiceChannel.join();
 
-    // We can use the connection to play the file.
-    const dispatcher = await connection.play(
-      path.join(__dirname, './temp/output1.wav'),
-      {
-        volume: 0.8,
-      },
-    );
+        // We can use the connection to play the file.
+        let dispatcher = await connection.play(path.join(__dirname, './temp/output1.wav'), {
+            volume: 0.8
+        });
 
-    dispatcher
-      .on('finish', async () => {
-        const dispatcher2 = await connection.play(
-          path.join(__dirname, './temp/output2.wav'),
-          {
-            volume: 0.8,
-          },
-        );
+        dispatcher.on('finish', async() => {
+                let dispatcher2 = await connection.play(path.join(__dirname, './temp/output2.wav'), {
+                    volume: 0.8
+                });
 
-        dispatcher2
-          .on('finish', async () => {
-            const dispatcher3 = await connection.play(
-              path.join(__dirname, './temp/output3.wav'),
-              {
-                volume: 0.8,
-              },
-            );
-            dispatcher3
-              .on('finish', async () => {
-                voiceChannel.leave();
-              }) // Log any stream errors that might occurr.
-              .on('error', (err) => {
+                dispatcher2.on('finish', async() => {
+                        let dispatcher3 = await connection.play(path.join(__dirname, './temp/output3.wav'), {
+                            volume: 0.8
+                        });
+                        dispatcher3.on('finish', async() => {
+                                voiceChannel.leave();
+                            }) // Log any stream errors that might occurr.
+                            .on('error', err => {
+                                console.error(err);
+                            });
+                    }) // Log any stream errors that might occurr.
+                    .on('error', err => {
+                        console.error(err);
+                    });
+            }) // Log any stream errors that might occurr.
+            .on('error', err => {
                 console.error(err);
-              });
-          }) // Log any stream errors that might occurr.
-          .on('error', (err) => {
-            console.error(err);
-          });
-      }) // Log any stream errors that might occurr.
-      .on('error', (err) => {
-        console.error(err);
-      });
-  },
+            });
+    }
 };
