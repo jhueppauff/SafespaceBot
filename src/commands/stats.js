ef5c9015d8089973bba020ee351210142863c31b
Tom Siewert <tom@siewert.io>
2021-03-02 21:35:13 +0100

    🔨 Please the linter
    Signed-off-by: Tom Siewert <tom@siewert.io>

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,27 +1,27 @@
 const { MessageEmbed } = require('discord.js');
 // Make sure to export properties
 module.exports = {
-    name: 'stats', // Command name (what's gonna be used to call the command)
+  name: 'stats', // Command name (what's gonna be used to call the command)
 
-    execute(client, message) {
-        // Construct info embed
-        const embed = new MessageEmbed()
-            .setTitle('About')
-            .setColor('RANDOM')
-            .setDescription('Stats about this bot')
-            .addField('Created', client.user.createdAt)
-            .addField(
-                'Heap Usage',
-                `${Math.round(process.memoryUsage().heapUsed / 1048576)}mb`,
-                true,
-            ) // 1048576 = size of an mb in bytes
-            .addField(
-                'Uptime',
-                new Date(process.uptime() * 1000).toISOString().slice(11, 19),
-                true,
-            )
-            .setFooter('Safespace Bot', client.user.displayAvatarURL);
-        // Send message
-        message.channel.send({ embed });
-    },
+  execute(client, message) {
+    // Construct info embed
+    const embed = new MessageEmbed()
+      .setTitle('About')
+      .setColor('RANDOM')
+      .setDescription('Stats about this bot')
+      .addField('Created', client.user.createdAt)
+      .addField(
+        'Heap Usage',
+        `${Math.round(process.memoryUsage().heapUsed / 1048576)}mb`,
+        true,
+      ) // 1048576 = size of an mb in bytes
+      .addField(
+        'Uptime',
+        new Date(process.uptime() * 1000).toISOString().slice(11, 19),
+        true,
+      )
+      .setFooter('Safespace Bot', client.user.displayAvatarURL);
+    // Send message
+    message.channel.send({ embed });
+  },
 };
